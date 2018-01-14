2723dbaaa9f2c7053a667a725e525c75a51e22c2
chivu55 <igeda.de@gmail.com>
2018-01-14 12:57:03 +0100

    Multiply calculate

--- DemoCalc/src/com/hfu/dilek/Calculation.java
+++ DemoCalc/src/com/hfu/dilek/Calculation.java
@@ -2,29 +2,42 @@ package com.hfu.dilek;
 
 public class Calculation {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            System.out.println("Beginn with mathematics");
+        System.out.println("Beginn with mathematics");
 
-            additionStep();
+        additionStep();
+
+        multipStep();
+    }
+
+    private static void multipStep() {
+
+        for (int i = 0; i < 7; i++) {
+            int random = createRandom();
+            System.out.println("Counter i =  " + i + ", Random number " + random + ",  Multiply: " + (i + random));
         }
 
-        private static void additionStep() {
+        System.out.println("");
+        System.out.println("End of Multiply");
+    }
 
-            for (int i = 0; i < 5; i++) {
+    private static void additionStep() {
 
-                int random = createRandom();
-                System.out.println("Counter i =  " + i + ", Random number " + random + ",  Addition: " + (i + random));
-            }
+        for (int i = 0; i < 5; i++) {
 
-            System.out.println("Ende");
+            int random = createRandom();
+            System.out.println("Counter i =  " + i + ", Random number " + random + ",  Addition: " + (i + random));
         }
 
-        private static int createRandom() {
-            // minimum 1 and maximum 50
-            int random = (int) (Math.random() * 50 + 1);
-            return random;
-        }
+        System.out.println("Ende");
+    }
+
+    private static int createRandom() {
+        // minimum 1 and maximum 50
+        int random = (int) (Math.random() * 50 + 1);
+        return random;
+    }
 }
 
 
